aa83e6951815f92314f82587bcb63bc212c80bd2
Ashraff Shaik <ashraff.x.shaik@kp.org>
2024-03-31 14:49:30 +0530

    commit 2 added hoodies and t-shirts as  json file

--- index.js
+++ index.js
@@ -1,18 +1,19 @@
-const app = require('express')();
-const PORT = 8080;
+const express = require('express');
+const app = express();
+const path = require('path');
+const PORT = 8090;
 
 app.listen(
     PORT,
-    () => console.log(`it is live on http://localhost:${PORT}`)
-)
-
-app.get('/tshirt', (req, res) => {
-    res.status(200).send({
-        tshirt: 'Blue Long Sleeve Polo',
-        size: 'X-Large'
-    }
-
-    )
-}
+    () => console.log(`Server is live on http://localhost:${PORT}`)
 );
 
+app.get('/tshirt', (req, res) => {
+    const filePath = path.join(__dirname, 'tshirt.json');
+    res.status(200).sendFile(filePath);
+});
+
+app.get('/hoodies', (req, res) => {
+    const filePath = path.join(__dirname, 'hoodies.json');
+    res.status(200).sendFile(filePath);
+});
